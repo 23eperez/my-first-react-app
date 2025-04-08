5938c95e73208312c5b43f0a47fe06c97cfbff3a
Eduardo Perez <ep3700299@gmail.com>
2025-04-08 15:28:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <div className=  "App">
+      <h1>
+        Hi, my name is Eduardo </h1>
+      <img 
+      src = '/smile.jpg'
+      alt = "me"
+      style = {{width : '200px',bordeRadius: '100px' }}
+      />
+    </div>
+
+   
+  );
+}
+
+export default App;
